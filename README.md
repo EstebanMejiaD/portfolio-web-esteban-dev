bbdcbb6d7c61d903ec57fddd5466ed6b3b1f2c85
Esteban Mejia <estebandeveloper10@gmail.com>
2022-12-05 22:52:54 -0500

    Aplicacion creada y lista para desarrollar

---
+++ README.md
@@ -0,0 +1,4 @@
+Hola, Soy Esteban Mejia y soy desarrollador web fullstack jr. 
+
+# Este es mi fortafolio profesional... y está en desarrollo!
+
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+        <h3>Hey. I'm Esteban Mejia and I'm a fullstack web developer.</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
