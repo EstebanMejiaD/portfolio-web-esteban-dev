51be8b8b9781f0bd965f01d9d98d855eed05d9f4
Esteban Mejia <estebandeveloper10@gmail.com>
2022-12-10 12:22:38 -0500

    Intento de arreglo de bug target = '_blank'

--- src/Components/MiniAreaContact.jsx
+++ src/Components/MiniAreaContact.jsx
@@ -7,29 +7,44 @@ const MiniAreaContact = () => {
         <div className="row">
           <p className="contact-p">Esteban Mejia</p>
           <p className="">full stack developer</p>
-         
         </div>
         <div className="row contenedor-links text-center">
-        <a
+          <a
             className="links-contact first-item-contact"
-            target="_blank"
+            rel="noreferrel"
             href="https://twitter.com/mejiaesteban08"
           >
             <i class="bi bi-twitter "></i>
           </a>
           <a
-            target="_blank"
+            rel="noreferrel"
             className="links-contact"
             href="https://api.whatsapp.com/send/?phone=573043112837&text&type=phone_number&app_absent=0"
           >
             <i class="bi bi-whatsapp "></i>
           </a>
-          <a target='_blank' className="links-contact" href="https://github.com/EstebanMejiaD"><i class="bi bi-github"></i></a>
-          <a target='_blank' className="links-contact" href="https://www.linkedin.com/in/esteban-andres-mej%C3%ADa-de-la-hoz-4515a318b/"><i class="bi bi-linkedin"></i></a>
-          
-         
-          
-          <a target='_blank' className="links-contact" href="https://www.linkedin.com/in/esteban-andres-mej%C3%ADa-de-la-hoz-4515a318b/"><i class="bi bi-facebook"></i></a>
+          <a
+            rel="noreferrel"
+            className="links-contact"
+            href="https://github.com/EstebanMejiaD"
+          >
+            <i class="bi bi-github"></i>
+          </a>
+          <a
+            rel="noreferrel"
+            className="links-contact"
+            href="https://www.linkedin.com/in/esteban-andres-mej%C3%ADa-de-la-hoz-4515a318b/"
+          >
+            <i class="bi bi-linkedin"></i>
+          </a>
+
+          <a
+            rel="noreferrel"
+            className="links-contact"
+            href="https://www.linkedin.com/in/esteban-andres-mej%C3%ADa-de-la-hoz-4515a318b/"
+          >
+            <i class="bi bi-facebook"></i>
+          </a>
         </div>
       </div>
     </div>
