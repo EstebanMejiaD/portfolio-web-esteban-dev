c44e04dcd62d039e641e1e5c2a4d1b56ac0d2810
Esteban Mejia <estebandeveloper10@gmail.com>
2022-12-10 14:18:56 -0500

    Animaciones del texto principal completo

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import './App.css';
 import Navbar from "./Components/Navbar";
 import Header  from './Components/Header';
 import MiniAreaContact from './Components/MiniAreaContact';
-
 import ScrollDown from './Components/ScrollDown';
 function App() {
   return (
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -8,14 +8,14 @@ const Header = () => {
           <div className="col-md-10 container-h1principal">
             <h1 className="fontAbril h1Principal">
               <div className="fontAbril typing-achine">Hey.</div>
-              I'm Esteban Mejia and I'm a passionate fullstack web developer jr.
+              <div className="fontAbril h1animation">I'm Esteban Mejia and I'm a passionate fullstack web developer jr.</div>
             </h1>
           </div>
-          <div>
-            {/* <div className="container-image">
+          {/* <div className="col-md-4">
+            <div className="container-image">
                 <img className='imagen-esteban' src="/images/fotoportafolio.png" alt="Esteban Mejia" />
-            </div> */}
-          </div>
+            </div> 
+          </div> */}
         </div>
       </div>
     </div>
